4bd9918c6876b42c0579d1d1478102520f70e9f7
tasnuvaairen <41597319+tasnuvaairen@users.noreply.github.com>
2020-02-07 13:31:21 -0500

    sqlalchemy_challenge

--- 10-Advanced-Data-Storage-and-Retrieval/Instructions/app.py
+++ 10-Advanced-Data-Storage-and-Retrieval/Instructions/app.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 
@@ -22,7 +23,7 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+##session = Session(engine)
 
 app = Flask (__name__)
 
@@ -38,57 +39,50 @@ def welcome ():
         f"/api/v1.0/temp/<start>/<end>\n"
 )
 
-@app.route("/api/v1.0/precipitation<br/>")
+@app.route("/api/v1.0/precipitation")
 def precipitation ():
+    session = Session(engine)
     last_one_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation_scores = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_one_year).all()
     precip = {date:prcp for date, prcp in precipitation_scores}
     return jsonify (precip)
 
-@app.route("/api/v1.0/stations<br/>")
+@app.route("/api/v1.0/stations")
 def stations ():
+    session = Session(engine)
     stat_no = session.query(Station.station).all()
     stat = list(np.ravel(stat_no))
     return jsonify (stat) 
 
-@app.route("/api/v1.0/tobs<br/>")
+@app.route("/api/v1.0/tobs")
 def temperature ():
-    last_one_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    station_temp = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= last_one_year ).all()
+    session = Session(engine)
+    last_one_year_2 = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    station_temp = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= last_one_year_2 ).all()
     temp = list(np.ravel(station_temp))
     return jsonify (temp) 
 
-@app.route("/api/v1.0/temp/<start><br/>")
 
-def start(start=None):
+#@app.route("/api/v1.0/temp/<start><br/>")
+#def start(start=None):
+@app.route("/api/v1.0/temp/<start_date>")
+def start(start_date):
+    session = Session(engine)
     sel = [Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation, func.sum(Measurement.prcp)]
     precipitation_results = session.query(*sel).filter(Measurement.station == Station.station).filter(Measurement.date >= start_date).all()
     temp_start= list(np.ravel(precipitation_results))
     return jsonify (temp_start)
 
- @app.route("/api/v1.0/temp/<start>/<end>)
- 
- def start(start=None):
+#@app.route("/api/v1.0/temp/<start>/<end>")
+#def start(start=None):
+@app.route("/api/v1.0/temp/<start_date>/<end_date>")
+def start_end(start_date, end_date):
+    session = Session(engine)
     sel = [Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation, func.sum(Measurement.prcp)]
     precipitation_results = session.query(*sel).filter(Measurement.station == Station.station).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
     temp_start= list(np.ravel(precipitation_results))
     return jsonify (temp_start) 
 
 
- if __name__ == '__main__':
-    app.run()   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run()
